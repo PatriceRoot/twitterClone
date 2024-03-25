16eaec55ed2206f578388663cfac188623923d96
PatriceKalwira <patricekalwira@gmail.com>
2024-03-25 19:29:38 +0200

    finish sprint2

--- src/components/tweet-editor/avatar.jsx
+++ src/components/tweet-editor/avatar.jsx
@@ -1,5 +1,10 @@
+import { Link } from "react-router-dom";
 import ProfilePhoto from "../../assets/images/profile.png";
 
-export default function Avatar() {
-  return <img className="avatar" src={ProfilePhoto} alt="avatar" />;
+export default function Avatar({ username }) {
+  return (
+    <Link to={`/bradley_`}>
+      <img className="avatar" src={ProfilePhoto} alt="avatar" />;
+    </Link>
+  );
 }
