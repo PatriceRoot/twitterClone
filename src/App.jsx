b97dd4ffc4eae5c6eaf6eba87eb677e00a56902d
PatriceKalwira <patricekalwira@gmail.com>
2024-04-16 09:55:47 +0200

    Making http request with axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,42 @@
-import Sidebar from "./components/sidebar.jsx";
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import useFetch from "./useFetch";
+import { useEffect, useState } from "react";
+import Sidebar from "./components/sidebar.jsx";
 import Trends from "./components/trends.jsx";
 import Follow from "./components/trends/follow.jsx";
 import Footer from "./components/footer.jsx";
-import { Outlet } from "react-router-dom";
 import { TweetContext } from "./contexts/tweets.js";
-import initial from "../src/data/initial-data.json";
 
-export default function App() {
-  const data = { ...initial };
+function App() {
+  const { data: tweetData } = useFetch("http://localhost:3000/tweets");
+  const [tweets, setTweets] = useState(null);
+  useEffect(() => setTweets(tweetData), [tweetData]);
+
+  const { data: currentData } = useFetch("http://localhost:3000/current-user");
+  const [current, setCurrent] = useState(null);
+  useEffect(() => setCurrent(currentData), [currentData]);
 
   return (
-    <>
-      <TweetContext.Provider value={data}>
-        <div style={{ flex: "0 0 25%" }}>
-          <Sidebar />
-        </div>
-        <div className="timeline">
-          <Outlet />
-        </div>
-        <div style={{ flex: "0 0 25%" }}>
-          <Trends />
-          <Follow />
-          <Footer />
-        </div>
-      </TweetContext.Provider>
-    </>
+    tweets &&
+    current && (
+      <>
+        <TweetContext.Provider value={{ tweets, current }}>
+          <div style={{ flex: "0 0 25%" }}>
+            <Sidebar />
+          </div>
+          <div className="timeline">
+            <Outlet />
+          </div>
+          <div style={{ flex: "0 0 25%" }}>
+            <Trends />
+            <Follow />
+            <Footer />
+          </div>
+        </TweetContext.Provider>
+      </>
+    )
   );
 }
+
+export default App;
--- src/components/tweet-editor/avatar.jsx
+++ src/components/tweet-editor/avatar.jsx
@@ -4,20 +4,10 @@ import { TweetContext } from "../../contexts/tweets";
 
 export default function Avatar() {
   const avatar = useContext(TweetContext);
+  console.log(avatar);
   return (
-    <Link to={`/${avatar["current-user"].userName}`} className="avatar">
-      <img src={avatar["current-user"].src} alt="Profil d'utilisateur" />
+    <Link to={`/${avatar["current"].userName}`} className="avatar">
+      <img src={avatar["current"].src} alt="Profil d'utilisateur" />
     </Link>
   );
 }
-
-// import { Link } from "react-router-dom";
-// import ProfilePhoto from "../../assets/images/profile.png";
-
-// export default function Avatar({ username }) {
-//   return (
-//     <Link to={`/bradley_`}>
-//       <img className="avatar" src={ProfilePhoto} alt="avatar" />
-//     </Link>
-//   );
-// }
